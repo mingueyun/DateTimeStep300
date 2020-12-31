336354baaec5b29ec18ee9e36f005cd556e98042
student <student@learncodinganywhere.com>
2020-12-30 17:58:48 -0800

    Add project files.

---
+++ DateTimeStep300/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeStep300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("give me number in hours you want to be reminded?");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(x));
+
+            Console.ReadLine();
+        }
+    }
+    
+}
